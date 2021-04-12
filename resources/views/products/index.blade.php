af96997cfc18bebcef8277ff3a55ee79d0aa89fd
Joseph Dubon <josephdubon@pm.me>
2021-04-11 18:55:03 -0700

    Displaying products

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+<h1>Products Page</h1>
+
+<p>{{ $products }}</p>
+
+
+</body>
+</html>
+@josephdubon
