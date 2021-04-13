a098cc5f0bbf9169b4f730d2d5731a3e16f53965
Joseph Dubon <josephdubon@pm.me>
2021-04-13 14:16:09 -0700

    Updated url image tag for storage dir and asset tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,7 @@
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                     <div class="col">
                         <div class="card shadow-sm">
-                            <img src="{{ url ('images/icon-box.jpg') }}" alt="">
+                            <img src="{{ asset ('storage/icon-box.jpg') }}" alt="">
 
                             <div class="card-body">
                                 <p class="card-text">This is a wider card with supporting text below as a natural
@@ -40,7 +40,7 @@
                     </div>
                     <div class="col">
                         <div class="card shadow-sm">
-                            <img src="{{ url ('images/icon-box.jpg') }}" alt="">
+                            <img src="{{ asset ('storage/icon-box.jpg') }}" alt="">
 
                             <div class="card-body">
                                 <p class="card-text">This is a wider card with supporting text below as a natural
@@ -57,58 +57,7 @@
                     </div>
                     <div class="col">
                         <div class="card shadow-sm">
-                            <img src="{{ url ('images/icon-box.jpg') }}" alt="">
-
-                            <div class="card-body">
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small class="text-muted">9 mins</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <img src="{{ url ('images/icon-box.jpg') }}" alt="">
-                            <div class="card-body">
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small class="text-muted">9 mins</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <img src="{{ url ('images/icon-box.jpg') }}" alt="">
-
-                            <div class="card-body">
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small class="text-muted">9 mins</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <img src="{{ url ('images/icon-box.jpg') }}" alt="">
+                            <img src="{{ asset ('storage/icon-box.jpg') }}" alt="">
 
                             <div class="card-body">
                                 <p class="card-text">This is a wider card with supporting text below as a natural
@@ -126,7 +75,23 @@
 
                     <div class="col">
                         <div class="card shadow-sm">
-                            <img src="{{ url ('images/icon-box.jpg') }}" alt="">
+                            <img src="{{ asset ('storage/icon-box.jpg') }}" alt="">
+                            <div class="card-body">
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                    <small class="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <img src="{{ asset ('storage/icon-box.jpg') }}" alt="">
 
                             <div class="card-body">
                                 <p class="card-text">This is a wider card with supporting text below as a natural
@@ -143,7 +108,25 @@
                     </div>
                     <div class="col">
                         <div class="card shadow-sm">
-                            <img src="{{ url ('images/icon-box.jpg') }}" alt="">
+                            <img src="{{ asset ('storage/icon-box.jpg') }}" alt="">
+
+                            <div class="card-body">
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                    <small class="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <img src="{{ asset ('storage/icon-box.jpg') }}" alt="">
 
                             <div class="card-body">
                                 <p class="card-text">This is a wider card with supporting text below as a natural
@@ -160,7 +143,24 @@
                     </div>
                     <div class="col">
                         <div class="card shadow-sm">
-                            <img src="{{ url ('images/icon-box.jpg') }}" alt="">
+                            <img src="{{ asset ('storage/icon-box.jpg') }}" alt="">
+
+                            <div class="card-body">
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                    <small class="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <img src="{{ asset ('storage/icon-box.jpg') }}" alt="">
 
                             <div class="card-body">
                                 <p class="card-text">This is a wider card with supporting text below as a natural
