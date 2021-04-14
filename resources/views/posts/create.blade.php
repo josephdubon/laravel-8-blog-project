334599930a9ec7fa5a8c080b99755cb58ea9b43a
Joseph Dubon <josephdubon@pm.me>
2021-04-13 20:22:57 -0700

    Created create post view

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <main>
+        <section class="py-5 text-center container">
+            <div class="row py-lg-5">
+                <div class="col-lg-6 col-md-8 mx-auto">
+                    <h1 class="fw-light">Create A Post Page</h1>
+                    <p class="lead text-muted">Something short and leading about the collection below—its contents, the
+                        creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it
+                        entirely.</p>
+
+                </div>
+            </div>
+        </section>
+
+        <div class="album py-5 bg-light">
+            <div class="container">
+
+                FORM HERE
+
+            </div>
+        </div>
+
+    </main>
+
+@endsection
