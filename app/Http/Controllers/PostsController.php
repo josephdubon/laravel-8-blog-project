7670b4a01599dd0288e367310eafa6ef7ce10ea7
Joseph Dubon <josephdubon@pm.me>
2021-04-13 20:17:24 -0700

    Created Posts controller and added some variables to loop over the front end on

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
     public function index()
     {
-        $id = 6;
+
+        $posts = Post::all();
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
+
         // Query Builder Methods
-        $posts = DB::table('posts')
+//        $posts = DB::table('posts');
 //            ->where('created_at', '>', now()->subDay())
 //            ->orWhere('title', 'Prof.')
 //            ->whereBetween('id', [1, 5])
@@ -21,10 +32,10 @@ class PostsController extends Controller
 //            ->orderBy('title', 'asc')
 //            ->latest()
 //            ->orderBy('created_at', 'desc')
-            // get first item that matches query
+        // get first item that matches query
 //            ->first();
-            // get all items
-            ->get();
+        // get all items
+//            ->get();
 
 //            ->insert([
 //                'title' => 'new post',
@@ -42,6 +53,78 @@ class PostsController extends Controller
 //            ->delete();
 
         // dump and die, dump it all
-        dd($posts);
+//        dd($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public
+    function destroy($id)
+    {
+        //
     }
 }
